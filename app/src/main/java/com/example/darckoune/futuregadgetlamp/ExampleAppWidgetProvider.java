bce920b6fa4a9eeb804b70ef6862f61cdcf7ee73
Darckoune <darckoune@gmail.com>
2018-11-12 11:20:41 +0100

    zone layout setup

--- app/src/main/java/com/example/darckoune/futuregadgetlamp/ExampleAppWidgetProvider.java
+++ app/src/main/java/com/example/darckoune/futuregadgetlamp/ExampleAppWidgetProvider.java
@@ -10,14 +10,26 @@ import android.widget.RemoteViews;
 
 public class ExampleAppWidgetProvider extends AppWidgetProvider {
     private static final String MyOnClick = "myOnClickTag";
+    private static final String MyOnClick2 = "myOnClickTag2";
+    private static final String MyOnClick3 = "myOnClickTag3";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId: appWidgetIds){
 
+            Log.i("ONUPDATE", "FOR");
+
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
-            views.setOnClickPendingIntent(R.id.example_widget_button,
-                    getPendingSelfIntent(context, MyOnClick));
+
+            RemoteViews button = new RemoteViews(context.getPackageName(), R.layout.zone);
+            button.setOnClickPendingIntent(R.id.onButton,
+                    getPendingSelfIntent(context, MyOnClick2));
+            views.addView(R.id.container, button);
+
+            RemoteViews button2 = new RemoteViews(context.getPackageName(), R.layout.zone);
+            button2.setOnClickPendingIntent(R.id.onButton,
+                    getPendingSelfIntent(context, MyOnClick3));
+            views.addView(R.id.container, button2);
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
@@ -33,8 +45,16 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
         if (MyOnClick.equals(intent.getAction())){
-            Log.e("WOW", "ON");
+            Log.i("WOW", "ON");
             new HttpTask().execute("http://home.darckoune.moe:8084/api/myGateway/Salon%20x3/ON");
+        }
+
+        if (MyOnClick2.equals(intent.getAction())){
+            Log.i("WOW", "2");
+        }
+
+        if (MyOnClick3.equals(intent.getAction())){
+            Log.i("WOW", "3");
         }
     }
 }
